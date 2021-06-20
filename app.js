8ef6cd06ff22033d45c8baa4fa049d5c97f9bb1b
Trevor Smart <trevor@leafythings.com>
2021-06-20 09:36:26 -0700

    bluh

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-alert("hi there");
+topKFrequent = function (nums, k) {
+    const output = [];
+    const counts = {};
+
+    //count the unique values found in the input
+    let biggest = 0;
+    for (num of nums) {
+        if (counts[num])
+            counts[num] += 1;
+        else 
+            counts[num] = 1;
+            
+        if (counts[num] > biggest) {
+            biggest = counts[num];
+            output.push(num);
+        }
+    }
+
+    //get the 2 largest numbers
+    return [output[output.length - 1], output[output.length - 2]];
+};
+
+console.log(topKFrequent([1, 1, 1, 1, 2, 2, 3], 2));
